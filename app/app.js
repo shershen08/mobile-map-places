3637204ae3b472720ce32dc2341cee6c7586a0d3
Mikhail Kuznetcov <mkuznetcov@effacts.com>
2015-04-15 18:09:12 +0200

    points are displayed  on map

--- app/app.js
+++ app/app.js
@@ -18,6 +18,10 @@ mapApp.config(function($stateProvider, $urlRouterProvider) {
             .state("add", {
           url: "/add",
           controller : 'homeCtrl'
+        }) 
+            .state("about", {
+          url: "/about",
+          controller : 'homeCtrl'
         })
 });
 
@@ -30,11 +34,48 @@ mapApp.config(function(uiGmapGoogleMapApiProvider) {
     });
 });
 
-mapApp.controller('homeCtrl', ['$scope', '$state', '$stateParams',
-      function ($scope, $state, $stateParams) {
+mapApp.constant('APP_SETTINGS', {
+  'centerPoint' :  [52.370216, 4.895168], //Amsterdam
+  'defaultZoom' :  13
+})
+
+mapApp.controller('homeCtrl', ['$scope', '$state', '$stateParams', '$http', 'APP_SETTINGS',
+      function ($scope, $state, $stateParams, $http, APP_SETTINGS) {
+
+    $http.get('app/map.geojson').then(function(data){
+
+      /*
+
+            { "type": "Feature", "geometry": { 
+                                            "type": "Point",
+                                            "coordinates": [ 4.900556802749634, 52.37315277095617 ] 
+                                            },
+                                  "properties": { "id": 1, "address": "todo" }
+            }
+
+      */
+      var pointsArray = [];
+      data.data.features.forEach(function(element, index){
+
+          var ret = {
+                      latitude    : element.geometry.coordinates[1],
+                      longitude   : element.geometry.coordinates[0],
+                      title       : element.properties.address,
+                      id          : element.properties.id
+                    };
+
+            pointsArray.push(ret);
+      })
 
 
-    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+      $scope.points = pointsArray;
+    })    
+
+    $scope.map = { center: { latitude: APP_SETTINGS.centerPoint[0], longitude: APP_SETTINGS.centerPoint[1] }, zoom: APP_SETTINGS.defaultZoom };
 
     $scope.title = $state.current.name;
+
+    $scope.markerClick  = function(item){
+      console.info(item);
+    }
 }])
