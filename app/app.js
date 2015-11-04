e3eabe812f1f3ad0f59f12732e7ed0bf7d993f2a
Mikhail Kuznetcov <mkuznetcov@effacts.com>
2015-11-04 16:56:53 +0100

    added geolocation button

--- app/app.js
+++ app/app.js
@@ -18,7 +18,7 @@ mapApp.config(function($stateProvider, $urlRouterProvider) {
             .state("add", {
           url: "/add",
           controller : 'homeCtrl'
-        }) 
+        })
             .state("about", {
           url: "/about",
           controller : 'homeCtrl'
@@ -40,37 +40,56 @@ mapApp.constant('APP_SETTINGS', {
   'version'     : '0.2'
 })
 
+mapApp.value('version', '0.2');
+
 mapApp.controller('homeCtrl', ['$scope', '$state', '$stateParams', '$http', 'APP_SETTINGS',
       function ($scope, $state, $stateParams, $http, APP_SETTINGS) {
 
-    $http.get('app/map-v0.2-.geojson').then(function(data){
+    $http.get('app/map-v0.2-.geojson').then(function(response){
 
-      /*
-
-            { "type": "Feature", "geometry": { 
-                                            "type": "Point",
-                                            "coordinates": [ 4.900556802749634, 52.37315277095617 ] 
-                                            },
-                                  "properties": { "id": 1, "address": "todo" }
-            }
-
-      */
       var pointsArray = [];
-      data.data.features.forEach(function(element, index){
+      response.data.features.forEach(function(element, index){
 
           var ret = {
                       latitude    : element.geometry.coordinates[1],
                       longitude   : element.geometry.coordinates[0],
-                      title       : 'dasdsddsa',
+                      title       : 'Point ' + index,
                       id          : index
                     };
 
             pointsArray.push(ret);
       })
-
-
       $scope.points = pointsArray;
-    })    
+    })
+
+    function requestGeoPosition(){
+      if (navigator.geolocation) {
+         return navigator.geolocation.getCurrentPosition(showPosition);
+      } else {
+         showPosition("Geolocation is not supported by this browser");
+      }
+    }
+
+    $scope.locateUser = function(){
+      requestGeoPosition();
+    }
+
+    function showPosition(details){
+      if(angular.isObject(details)){
+          addItemToMap(details.coords);
+      }
+    }
+
+    function addItemToMap(coordinates){
+        var ret = {
+                    latitude    : coordinates.latitude,
+                    longitude   : coordinates.longitude,
+                    title       : 'You',
+                    id          : $scope.points.length
+                  };
+        $scope.points.push(ret);
+        $scope.$apply();
+    }
 
     $scope.map = { center: { latitude: APP_SETTINGS.centerPoint[0], longitude: APP_SETTINGS.centerPoint[1] }, zoom: APP_SETTINGS.defaultZoom };
 
